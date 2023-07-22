e6c3beeeeeaefe5e750b5df755617445354e3d85
praveenh001 <124371790+praveenh001@users.noreply.github.com>
2023-07-22 22:05:42 +0530

    Update library.py

--- phy.py
+++ phy.py
@@ -1,6 +1,6 @@
 #library system
 books = []
-def entry():
+def entry(): #to input the data of books
     code = int(input("Enter the book code: "))
     author = input("Enter the author name: ")
     book = input("Enter the book name: ")
@@ -8,12 +8,12 @@ def entry():
     book_data = [code, author, book, quantity]
     return book_data
 
-def add():
+def add(): #to add books
     book_data = entry()
     books.append(book_data)
     print("Book added successfully.")
 
-def dele():
+def delete(): #to delete books
     code_to_delete = int(input("Enter the book code to delete: "))
     for book_data in books:
         if book_data[0] == code_to_delete:
@@ -22,7 +22,7 @@ def dele():
             return
     print("Invalid book code. Book not found.")
 
-def issue():
+def issue(): #to issue books to students 
     code_to_issue = int(input("Enter the book code to issue: "))
     for book_data in books:
         if book_data[0] == code_to_issue:
@@ -35,7 +35,7 @@ def issue():
                 return
     print("Invalid book code. Book not found.")
 
-def reci():
+def reci(): #to recive book back from students
     code_to_receive = int(input("Enter the book code to receive: "))
     for book_data in books:
         if book_data[0] == code_to_receive:
@@ -44,8 +44,8 @@ def reci():
             return
     print("Invalid book code. Book not found.")
 
-for i in range(100):
-    print("\n\nWELCOME TO LIBRARY\n")
+for i in range(100):#for repeating the program
+    print("\n\nWELCOME TO LIBRARY\n") # body of the program
     print("1. Add books")
     print("2. Delete books")
     print("3. Issue books")
@@ -55,7 +55,7 @@ for i in range(100):
     if op == 1:
         add()
     elif op == 2:
-        dele()
+        delete()
     elif op == 3:
         issue()
     elif op == 4:
